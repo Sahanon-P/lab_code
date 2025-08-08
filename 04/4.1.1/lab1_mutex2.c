6c839a5678aaef30cddcaf1adcce4baa91a214f9
Sahanon Phisetpakasit <sahanon.dev@gmail.com>
2025-08-08 14:00:16 +1200

    lab4

---
+++ 04/4.1.1/lab1_mutex2.c
@@ -0,0 +1,62 @@
+/* **************** LDD:1.0 s_12/lab1_mutex2.c **************** */
+/*
+ * The code herein is: Copyright Jerry Cooperstein, 2009
+ *
+ * This Copyright is retained for the purpose of protecting free
+ * redistribution of source.
+ *
+ *     URL:    http://www.coopj.com
+ *     email:  coop@coopj.com
+ *
+ * The primary maintainer for this code is Jerry Cooperstein
+ * The CONTRIBUTORS file (distributed with this
+ * file) lists those known to have contributed to the source.
+ *
+ * This code is distributed under Version 2 of the GNU General Public
+ * License, which you should have received with the source.
+ *
+ */
+/*
+ * Semaphore Contention
+ *
+ * second and third module to test semaphores
+ @*/
+
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/mutex.h>
+#include <asm/atomic.h>
+#include <linux/errno.h>
+
+extern struct mutex my_mutex;
+
+static char *modname = __stringify(KBUILD_BASENAME);
+
+static int __init my_init(void)
+{
+	printk(KERN_INFO "Trying to load module %s\n", modname);
+	printk(KERN_INFO "\n%s start mutex state: %d lock owner: %u\n", modname,
+	       mutex_is_locked(&my_mutex), atomic_read(&my_mutex.owner));
+
+	/* COMPLETE ME */
+	/* lock my_mutex */
+	printk(KERN_INFO "\n%s mutex locked state: %d lock owner: %u\n",
+	       modname, mutex_is_locked(&my_mutex), atomic_read(&my_mutex.owner));
+
+	return 0;
+}
+
+static void __exit my_exit(void)
+{
+	/* COMPLETE ME */
+	/* unlock my_mutex */
+	printk(KERN_INFO "\n%s mutex end state: %d lock owner: %u\n",
+	       modname, mutex_is_locked(&my_mutex), atomic_read(&my_mutex.owner));
+}
+
+module_init(my_init);
+module_exit(my_exit);
+
+MODULE_AUTHOR("Tatsuo Kawasaki");
+MODULE_DESCRIPTION("LDD:1.0 s_12/lab1_mutex2.c");
+MODULE_LICENSE("GPL v2");
